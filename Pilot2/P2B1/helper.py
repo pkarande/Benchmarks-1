2939645fef163764a2986c8b13b71d7d779123d7
pkarande <karande1@.llnl.gov>
2018-01-08 11:52:37 -0800

    commenting code

--- Pilot2/P2B1/helper.py
+++ Pilot2/P2B1/helper.py
@@ -63,6 +63,15 @@ def append_nbrs_relative(x, nbrs, num_nbrs):
     '''
     Appends the neighbors to each molecule in the frame
     Also, uses x, y, z positions relative to the center of mass of the molecule.
+
+    Args:
+    x: data of shape (num_molecules, num_beads, features)
+    nbrs: neighbor index of molecules of shape (num_molecules, 100)
+    num_nbrs: int, number of neighbors to append
+
+    Returns:
+    x_wNbrs: concatenated features of all neighbors of shape (num_molecules, (num_nbrs+1)*num_beads*num_feature)
+
     '''
     new_x_shape = np.array((x.shape[0], np.prod(x.shape[1:])))
     new_x_shape[1] *= num_nbrs+1
@@ -95,6 +104,14 @@ def append_nbrs_relative(x, nbrs, num_nbrs):
 def append_nbrs(x, nbrs, num_nbrs):
     '''
     Appends the neighbors to each molecule in the frame
+
+    Args:
+    x: data of shape (num_molecules, num_beads*features)
+    nbrs: neighbor index of molecules of shape (num_molecules, 100)
+    num_nbrs: int, number of neighbors to append
+
+    Returns:
+    x_wNbrs: concatenated features of all neighbors of shape (num_molecules, (num_nbrs+1)*num_beads*num_feature)
     '''
     new_x_shape = np.array(x.shape)
     new_x_shape[1] *= num_nbrs+1
