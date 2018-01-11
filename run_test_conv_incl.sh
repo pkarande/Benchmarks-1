62fd30aabc5a711e736c626ef46b32e6d5de093a
Piyush Karande <karande1@llnl.gov>
2018-01-11 15:59:29 -0800

    changes to imports

--- run_test_conv_incl.sh
+++ run_test_conv_incl.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-cd /p/gscratchr/karande1/
-. spack/share/spack/setup-env.sh
-
-spack activate py-ipython
-spack activate py-keras
-spack activate py-tqdm
-spack activate py-scikit-learn
-spack activate py-requests
-
-module load py-ipython-5.1.0-gcc-4.8.5-l5wsryf
-module load python-2.7.13-gcc-4.8.5-mrskb7k
+. ~/spack-activate.sh
 
 cd /p/gscratchr/karande1/Benchmarks/Pilot2/P2B1/
 DATE=`date +%m%d%Y_%H%M%S`
