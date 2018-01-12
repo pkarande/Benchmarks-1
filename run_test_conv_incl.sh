fa9a79bd4714c479fa70d0fb0e098529139284e8
Piyush Karande <karande1@llnl.gov>
2018-01-12 14:41:04 -0800

    minor updates

--- run_test_conv_incl.sh
+++ run_test_conv_incl.sh
@@ -5,6 +5,6 @@ cd /p/gscratchr/karande1/Benchmarks/Pilot2/P2B1/
 DATE=`date +%m%d%Y_%H%M%S`
 save_path="/p/gscratchr/karande1/Benchmarks/Pilot2/P2B1/molecular_AE_results/$DATE"
 mkdir $save_path
-mpirun -np 1 ipython -i p2b1_baseline_keras2_mol_AE.py -- --save-dir $save_path --case Full --conv-AE --include-type --config-file p2b1_default_model.txt
+mpirun -np 1 ipython -i p2b1_baseline_keras2_mol_AE.py -- --save-dir $save_path --data-set 3k_run16 --case Full --conv-AE --include-type --config-file p2b1_default_model.txt
 
 echo 'Done'
